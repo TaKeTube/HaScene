ec8631e9dfa0b70ecf37ba2f38edeada220836d3
TaKeTube <jamie.zimu.guan@gmail.com>
2022-12-05 03:12:47 +0000

    light direction fixed

--- src/HaRender.hs
+++ src/HaRender.hs
@@ -17,6 +17,9 @@ import           Linear
 import           HaScene
 --   show :: Mesh -> String
 
+-- Chirality: Right-handed
+-- Camera always look at -Z direction no matter which space it is in
+
 getNorm :: Triangle -> V3 Float
 getNorm (V3 v0 v1 v2) = normalize $ cross (normalize $ v0 - v1) (normalize $ v0 - v2)
 
@@ -24,18 +27,6 @@ sortTriangle :: Triangle -> Triangle
 sortTriangle (V3 v0 v1 v2) = let
     [v0', v1', v2'] = sortBy (\(V3 _ y1 _) (V3 _ y2 _) -> compare y1 y2) [v0, v1, v2]
     in V3 v0' v1' v2'
-
--- viewMatrix :: Camera -> M44 Float
--- viewMatrix cam = let
---     rotM = eulerMatrix (_angle cam)
---     invRotM = transpose rotM
---     pos = _pos cam
---     V3 vx vy vz = - (invRotM !* pos)
---     V3 (V3 mxx mxy mxz) (V3 myx myy myz) (V3 mzx mzy mzz) = invRotM
---     in V4   (V4 mxx mxy mxz vx)
---             (V4 myx myy myz vy)
---             (V4 mzx mzy mzz vz)
---             (V4 0   0   0   1 )
 
 viewMatrix :: Camera -> M44 Float
 viewMatrix cam = let
@@ -51,25 +42,6 @@ viewMatrix cam = let
             (V4 mzx mzy mzz vz)
             (V4 0   0   0   1 )
 
--- eulerMatrix :: V3 Float -> M33 Float
--- eulerMatrix (V3 a b c) = let
---     cosa = cos a
---     sina = sin a
---     cosb = cos b
---     sinb = sin b
---     cosc = cos c
---     sinc = sin c
---     ma = V3 (V3 cosa    (-sina) 0      )
---             (V3 sina    cosa    0      )
---             (V3 0       0       1      )
---     mb = V3 (V3 1       0       0      )
---             (V3 0       cosb    (-sinb))
---             (V3 0       sinb    cosb   )
---     mc = V3 (V3 cosc    (-sinc) 0      )
---             (V3 sinc    cosc    0      )
---             (V3 0       0       1      )
---     in mc !*! mb !*! ma
-
 projMatrix :: Float -> Float -> Float -> Float -> M44 Float
 projMatrix fov aspectRatio zNear zFar = let
     invTan = 1 / tan (fov / 180 * pi * 0.5)
@@ -80,11 +52,16 @@ projMatrix fov aspectRatio zNear zFar = let
             (V4 0                    0       1                0                )
 
 transVec3 :: M44 Float -> V3 Float -> V3 Float
-transVec3 m (V3 x y z) = normalizePoint (m !* V4 x y z 1)
+transVec3 m (V3 x y z) = let
+    V4 x' y' z' _ = m !* V4 x y z 0
+    in V3 x' y' z'
+
+transPoint :: M44 Float -> V3 Float -> V3 Float
+transPoint m (V3 x y z) = normalizePoint (m !* V4 x y z 1)
 
 transTriangle :: M44 Float -> Triangle -> Triangle
 transTriangle mvpM (V3 v0 v1 v2) =
-    V3 (transVec3 mvpM v0) (transVec3 mvpM v1) (transVec3 mvpM v2)
+    V3 (transPoint mvpM v0) (transPoint mvpM v1) (transPoint mvpM v2)
 
 faceCulling :: [(Triangle, V3 Float)] -> [(Triangle, V3 Float)]
 faceCulling ts = let
@@ -107,7 +84,7 @@ fragShader :: V3 Float -> V3 Float -> Char
 fragShader light n = let
     colorMap = ".,-~:;!*=#$@"
     colorLen = length colorMap - 1
-    idx = round (int2Float colorLen * max 0 (dot (-light) n))
+    idx = max 0 $ round (int2Float colorLen * max 0 (dot (-light) n))
     -- idx = round (int2Float colorLen * (dot (-light) n * 0.5 + 0.5))
     in colorMap !! idx
 
@@ -126,8 +103,8 @@ berycentric2D x y (V3 (V3 x0 y0 _) (V3 x1 y1 _) (V3 x2 y2 _)) = let
     w = 1 - u - v
     in V3 u v w
 
-rasterize :: Int -> Int -> (Triangle, V3 Float) -> [((Int, Int), Float, Char)]
-rasterize w h (t@(V3 v0 v1 v2), n) = let
+rasterize :: Int -> Int -> (V3 Float -> Char) -> (Triangle, V3 Float) -> [((Int, Int), Float, Char)]
+rasterize w h fShader (t@(V3 v0 v1 v2), n) = let
     -- sort triangle verts by y
     [V3 xMin yMin zMin, V3 xMid yMid zMid, V3 xMax yMax zMax] =
         sortBy (\(V3 _ y1 _) (V3 _ y2 _) -> compare y1 y2) [v0, v1, v2]
@@ -162,13 +139,12 @@ rasterize w h (t@(V3 v0 v1 v2), n) = let
     interpZ x y = let
         beryCoord = berycentric2D x y t
         in perspectInterp (V3 zMin zMid zMax) beryCoord (V3 zMin zMid zMax)
-    -- Just Use Flat Shader
-    -- n = normalize $ cross v0 v1
-    color = fragShader (normalize (V3 (-1) 0 (-0.5))) n
+    -- apply fragment shader
+    color = fShader n
     -- index range of y
     yyd = y2yy (yMin + dy / 2)
     yyu = y2yy (yMax - dy / 2)
-    -- Generate pixels
+    -- generate pixels
     in [((xx, yy), interpZ (xx2x xx) (yy2y yy), color) | yy <- [yyd..yyu], let (xxl,xxr) = xxBound yy, xx <- [xxl..xxr]]
 
 render :: Int -> Int -> [Mesh] -> Camera -> String
@@ -177,7 +153,12 @@ render w h ms cam = elems $ runSTUArray $ do
     zbuf <- newArray ((0, 0),(h-1, w-1)) (-1.0/0.0) :: ST s (STUArray s (Int,Int) Float)
     -- frame buffer for pixels
     fbuf <- newArray ((0, 0),(h-1, w)) ' '       :: ST s (STUArray s (Int,Int) Char)
-    let pixels = concatMap (rasterize w h) $ faceCulling $ concatMap (vertShader cam) ms
+    -- set fragment shader
+    let light = normalize (V3 (-1) 0 (-0.5))
+    let viewM = viewMatrix cam
+    let light' = normalize $ transVec3 viewM light
+    -- rasterize triangles
+    let pixels = concatMap (rasterize w h (fragShader light')) $ faceCulling $ concatMap (vertShader cam) ms
     mapM_ (\((xx, yy), z, c) -> do
             z0 <- readArray zbuf (yy,xx)
             if z > z0 && z < 0
